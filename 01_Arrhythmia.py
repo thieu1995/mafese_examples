e050f7e83883d7a3d9e23a2f93106911e108df89
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-07-14 09:42:59 +0700

    Add script

---
+++ 01_Arrhythmia.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 08:44, 14/07/2023 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+from mafese import get_dataset, MultiMhaSelector
+
+
+PATH_SAVE = "history/Arrhythmia"
+EPOCH = 200
+POP_SIZE = 50
+TEST_SIZE = 0.2
+ESTIMATOR = "knn"
+
+data = get_dataset("Arrhythmia")
+data.split_train_test(test_size=TEST_SIZE)
+
+list_optimizers = ("BaseGA", "OriginalHGS", "OriginalARO", "OriginalEFO", "OriginalTLO", "BaseSMA", "AugmentedAEO", "OriginalRUN")
+list_paras = [
+    {"name": "GA", "epoch": EPOCH, "pop_size": POP_SIZE},
+    {"name": "HGS", "epoch": EPOCH, "pop_size": POP_SIZE},
+    {"name": "ARO", "epoch": EPOCH, "pop_size": POP_SIZE},
+    {"name": "EFO", "epoch": EPOCH, "pop_size": POP_SIZE},
+    {"name": "TLO", "epoch": EPOCH, "pop_size": POP_SIZE},
+    {"name": "SMA", "epoch": EPOCH, "pop_size": POP_SIZE},
+    {"name": "AAEO", "epoch": EPOCH, "pop_size": POP_SIZE},
+    {"name": "RUN", "epoch": EPOCH, "pop_size": POP_SIZE},
+]
+feat_selector = MultiMhaSelector(problem="classification", estimator=ESTIMATOR,
+                            list_optimizers=list_optimizers, list_optimizer_paras=list_paras,
+                            transfer_func="vstf_01", obj_name="AS")
+
+feat_selector.fit(data.X_train, data.y_train, n_trials=10, n_jobs=10, verbose=False, save_path=PATH_SAVE)
+feat_selector.export_boxplot_figures()
+feat_selector.export_convergence_figures()
+
+feat_selector.evaluate(estimator=ESTIMATOR, data=data, metrics=["AS", "PS", "RS"], save_path=PATH_SAVE, verbose=False)
