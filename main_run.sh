c153bf877abdcc1d199a49da19feb48bae95bad0
Thieu <nguyenthieu2102@gmail.com>
2023-07-16 00:49:40 +0700

    Update results

---
+++ main_run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+python 02_BreastEW.py &&
+python 03_Digits.py &&
+python 04_HillValley.py &&
+python 05_Ionosphere.py &&
+python 06_Madelon.py &&
+python 07_WaveformEW.py &&
+python 08_diabetes.py &&
+python 09_gauss5012.py &&
+python 10_gauss7517.py
+
